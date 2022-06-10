2878532da1895a87ccf1095709b2f8a85fd2dae2
LulianoM <martinslucianofigueira@gmail.com>
2022-06-09 21:05:29 -0300

    create-handlers-and-controllers-get-grafos

---
+++ application/controllers/grafosGet.py
@@ -0,0 +1,9 @@
+class GrafosControllerGet:
+    
+    @staticmethod
+    def GetAll():
+        return 200
+
+    @staticmethod
+    def GetByID(level, id):
+        return level, id
---
+++ application/handlers/grafos.py
@@ -0,0 +1,21 @@
+
+from flask import Blueprint,jsonify
+
+from application.controllers.grafosGet import GrafosControllerGet
+
+blueprint = Blueprint("grafos", __name__)
+
+
+@blueprint.route("/grafos", methods=["GET"])
+def GetAll():
+    value = GrafosControllerGet.GetAll()
+    return jsonify({"grafos": "ok"}), value
+    
+@blueprint.route("/grafos/<level>/<id>", methods=["GET"])
+def GetByID(level, id):
+    level_, id_ = GrafosControllerGet.GetByID(level , id)
+    return jsonify({"level": level_, "id": id_}), 200
+
+@blueprint.route("/grafos", methods=["POST"])
+def Create():
+    return jsonify({"grafos post": "ok"}), 200
